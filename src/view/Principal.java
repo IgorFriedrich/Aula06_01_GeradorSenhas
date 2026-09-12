9ab96180705717bd99fa2f13934371ff84e3acef
Igor Friedrich <igorleonardo96@gmail.com>
2026-09-12 20:31:49 -0300

    Adiciona uso de IGeradorSenha e gera senhas fortes

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,15 +1,25 @@
 package view;
+// Alunos:
+// Igor Friedrich
+// Caroline Sanches
 
-import business.GeradorSenha;
+import business.GeradorSenhaForte;
+import business.IGeradorSenha;
 
 public class Principal {
 
     public static void main(String[] args) {
 
-        GeradorSenha gerador;
+        IGeradorSenha gerador;
 
         // TODO:
         // Gerar duas senhas fortes
+        gerador = new GeradorSenhaForte(12);
+        System.out.println("Senha forte 1: " + gerador.gerarSenha());
+        
+        gerador = new GeradorSenhaForte(16);
+        System.out.println("Senha forte 2: " + gerador.gerarSenha());
+        // TODO:
         // Gerar dois PINs
         // Utilizar a variável "gerador" para demonstrar polimorfismo
 
