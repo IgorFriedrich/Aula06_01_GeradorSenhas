56b72500baa05afb82f83ffb24375ab9adc6a09b
Igor Friedrich <igorleonardo96@gmail.com>
2026-09-12 20:16:22 -0300

    Implementa geração de senha forte

--- src/business/GeradorSenhaForte.java
+++ src/business/GeradorSenhaForte.java
@@ -6,13 +6,28 @@ public class GeradorSenhaForte extends GeradorSenha {
 
     public GeradorSenhaForte(int tamanho) {
         // TODO validar tamanho mínimo
+        if (tamanho < 12) {
+            throw new IllegalArgumentException("A senha deve ter no mínimo 12 caracteres.");
+    }
         this.tamanho = tamanho;
     }
 
     @Override
     public String gerarSenha() {
         // TODO implementar
-        return null;
+          StringBuilder senha = new StringBuilder();
+
+        senha.append(sortearCaractere(MAIUSCULAS));
+        senha.append(sortearCaractere(MINUSCULAS));
+        senha.append(sortearCaractere(NUMEROS));
+        senha.append(sortearCaractere(ESPECIAIS));
+
+        String todosCaracteres = MAIUSCULAS + MINUSCULAS + NUMEROS + ESPECIAIS;
+
+        while (senha.length() < tamanho) {
+            senha.append(sortearCaractere(todosCaracteres));
+        }
+        return embaralhar(senha.toString());
     }
     
 }
