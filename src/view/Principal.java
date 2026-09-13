839cd19086452ab216d83d12d00e9537a13450f0
Caroline Sanches <sanches.codedev@gmail.com>
2026-09-12 22:38:37 -0300

    feat: adiciona geracao de PINs e demonstra polimorfismo na Principal

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -4,6 +4,7 @@ package view;
 // Caroline Sanches
 
 import business.GeradorSenhaForte;
+import business.GeradorPin; 
 import business.IGeradorSenha;
 
 public class Principal {
@@ -13,15 +14,23 @@ public class Principal {
         IGeradorSenha gerador;
 
         // TODO:
-        // Gerar duas senhas fortes
+        // Gerar duas senhas fortes (Feito pelo Igor)
         gerador = new GeradorSenhaForte(12);
         System.out.println("Senha forte 1: " + gerador.gerarSenha());
         
         gerador = new GeradorSenhaForte(16);
         System.out.println("Senha forte 2: " + gerador.gerarSenha());
+        
+        
         // TODO:
         // Gerar dois PINs
-        // Utilizar a variável "gerador" para demonstrar polimorfismo
+        // Utilizar a variável "gerador" para demonstrar polimorfismo (Feito por Carol)
+        
+        System.out.println("\n--- Gerando PINs ---");
+        gerador = new GeradorPin(); 
+        
+        System.out.println("PIN 1: " + gerador.gerarSenha());
+        System.out.println("PIN 2: " + gerador.gerarSenha());
 
     }
 }
